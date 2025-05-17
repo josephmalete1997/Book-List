0b56b7f129b5c2397cdd59285f746c27bc25a8be
josephmalete1997 <josephmalete75@gmail.com>
2025-05-17 13:29:33 +0200

    new updates

--- js/app.js
+++ js/app.js
@@ -4,19 +4,19 @@ import { viewBook } from "./helper_functions.js";
 const { bookDetails, booksPanel, genrePanel, overlay, closeBtn } = elements;
 
 const genreList = [
-  { name: "All", color: "#6c757d" },
-  { name: "Fiction", color: "#007bff" },
-  { name: "Dystopian", color: "#343a40" },
-  { name: "Fantasy", color: "#6f42c1" },
-  { name: "Romance", color: "#e83e8c" },
-  { name: "Adventure", color: "#fd7e14" },
-  { name: "Gothic", color: "#495057" },
-  { name: "Mystery", color: "#20c997" },
-  { name: "Psychological", color: "#17a2b8" },
-  { name: "Epic", color: "#6610f2" },
-  { name: "Non-fiction", color: "#28a745" },
-  { name: "Horror", color: "#dc3545" },
-  { name: "Science Fiction", color: "#0dcaf0" },
+  "All",
+  "Fiction",
+  "Dystopian",
+  "Fantasy",
+  "Romance",
+  "Adventure",
+  "Gothic",
+  "Mystery",
+  "Psychological",
+  "Epic",
+  "Non-fiction",
+  "Horror",
+  "Science Fiction",
 ];
 
 function getAllBooks(data) {
@@ -33,6 +33,7 @@ function getAllBooks(data) {
     book.id = item.id;
     book.innerHTML = `
             <span class="genre-tag">${item.genre}</span>
+            <span class="book-tool-tip"><i class="fa-solid fa-circle-info"></i> View details</span>
             <h3>${
               item.title.length > 20
                 ? item.title.slice(0, 20) + "..."
@@ -92,13 +93,13 @@ fetch("js/data.json", {})
     genreList.forEach((item) => {
       const genre = document.createElement("div");
       genre.className = "genre-btn";
-      genre.id = item.name;
+      genre.id = item;
       genre.addEventListener("click", () => {
         removeActiveGenre();
         genre.classList.add("active-genre");
-        getBooksByGenre(data, item.name);
+        getBooksByGenre(data, item);
       });
-      genre.innerHTML = item.name;
+      genre.innerHTML = item;
       genrePanel.append(genre);
     });
   })
